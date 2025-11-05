2ddbb4b9fab0c5ce5cd2f85392ee54d07e0a3751
Molnár Attila <molnar.attila@szlgbp.hu>
2025-11-05 11:53:05 +0100

    into yoursweeper, models.py

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Jatek(models.Model):
+
+    aknaszam = models.PositiveIntegerField()
+    egyik = models.ForeignKey(User, on_delete=models.PROTECT, related_name='egyik')
+    masik = models.ForeignKey(User, on_delete=models.PROTECT, null=True, blank=True, related_name='masik')
+    nyertes = models.ForeignKey(User, on_delete=models.PROTECT, null=True, blank=True, related_name='nyertes')
+    nev = models.CharField(max_length=100)
+
+   
+    class Meta:
+        verbose_name = "Játék"
+        verbose_name_plural = "Játékok"
+
+    def __str__(self):
+        return f'{self.nev}: {self.egyik} ⚔ {self.masik} '
